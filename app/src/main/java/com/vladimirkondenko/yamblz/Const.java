aec30ae9edb22597da1c2a48fe0fdd9fe60a9a4b
Vladimir Kondenko <kondenko2011@gmail.com>
2017-03-15 22:59:36 +0300

    Add API key constant

---
+++ app/src/main/java/com/vladimirkondenko/yamblz/Const.java
@@ -0,0 +1,7 @@
+package com.vladimirkondenko.yamblz;
+
+public final class Const {
+
+    public static final String API_KEY = "trnsl.1.1.20170315T172313Z.162bccb965880b7c.9feba3a457e1a4989e1bb689842c94d6bc9186e4";
+
+}
